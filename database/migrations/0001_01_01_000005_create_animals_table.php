fb2faa9d7ed3a86de45e9a94c5e20563301d5a2c
AgUsBF <agustinbeceyro@gmail.com>
2026-06-15 13:27:07 -0300

    Feat: Agregado de migraciones de roles, species y animals

---
+++ database/migrations/0001_01_01_000005_create_animals_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('animals', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->date('birth_date')->nullable();
+            $table->string('sex', 10);
+            $table->boolean('is_sterilized')->default(false);
+            $table->text('comment');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('animals');
+    }
+};
